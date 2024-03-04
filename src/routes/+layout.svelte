594599512500e7389376c0c0102c5167665a976c
Abhay Kalsi <abhaykalsi@Abhays-MacBook-Air.local>
2024-03-04 18:34:06 -0500

    svh

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 </header>
 
 <div class="relative">
-	<main class="relative z-10 min-h-dvh bg-white px-4 pb-4 rounded-b-xl">
+	<main class="relative z-10 min-h-svh bg-white px-4 pb-4 rounded-b-xl">
 		<slot />
 	</main>
 	<footer class="sticky bottom-0 z-0 h-40 w-full bg-black">
