acc5fe8f78051fecdad39e1c83325270c858880a
Abhay Kalsi <abhaykalsi@Abhays-MacBook-Air.local>
2024-03-05 10:09:44 -0500

    mobile breakpoint

--- src/routes/information/+page.svelte
+++ src/routes/information/+page.svelte
@@ -6,7 +6,7 @@
 	<h1 class="mt-8 border-b text-[4rem]">Information</h1>
 	<div class="mt-16 flex w-full">
 		<div class="w-1/3 md:w-2/5">
-			<h3 class="text-[1.5rem]">( Contact )</h3>
+			<h3 class="text-[1rem] md:text-[1.5rem]">( Contact )</h3>
 		</div>
 		<span class="w-2/3 max-w-[30ch] text-[2rem] md:text-[3rem]">
 			<span class="opacity-60"> Have&nbsp;Brain,&nbsp;Will&nbsp;Travel. </span>
