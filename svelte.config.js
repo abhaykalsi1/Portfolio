abe6204d3b00b9aad8d901119dc4d553c75bc6e6
Abhay Kalsi <abhaykalsi@Abhays-MacBook-Air.local>
2024-03-05 10:26:04 -0500

    vercel fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
-import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: adapter(),
-    }
+		adapter: adapter({
+			// see below for options that can be set here
+		})
+	}
 };
 
 export default config;
